c566316feba429ddc33546d1fd0ae72578a8a5d2
Niko Meister <25558975+dieserNiko@users.noreply.github.com>
2022-12-07 21:21:11 +0100

    Battery Temperature!
    Added battery temperature sensor readings, updated font to VWTextOfficeRegular to (almost) match the font in the active info display of e-Golf, changed the update and (while charging) changing interval from 10 to 7 seconds, added variables to debug different values easily.

--- obdscreen.py
+++ obdscreen.py
@@ -15,13 +15,13 @@ import obd
 from obd import OBDCommand
 from obd.utils import bytes_to_int
 
-# Set screenmode and which value to show by default after starting the car
 screenmode = "percscreen"
 screen = "perc"
 
 # Activate Debugmode to show custom percentage on display while not connected to car
 DEBUG = False
-DEBUGPERC = 69
+DEBUGPERC = 100
+DEBUGTEMP = 45
 
 # AdaFruit Display Raspberry Pi pin configuration:
 RST = None     # on the PiOLED this pin isnt used
@@ -54,14 +54,22 @@ draw = ImageDraw.Draw(image)
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
 # First define some constants to allow easy resizing of shapes.
-padding = 0 #-2
+padding = -6 #-2
 top = padding
 bottom = height-padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
 # Load font.
-font = ImageFont.truetype('/root/VAGRoundedRegular.ttf', 38)
+font = ImageFont.truetype('/root/VWTextOfficeRegular.ttf', 38)
+smallfont = ImageFont.truetype('/root/VWTextOfficeRegular.ttf', 15)
+
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+draw.text((x, top), str("booting..."), font=ImageFont.truetype('/root/VWTextOfficeRegular.ttf', 35), fill=255)
+
+disp.image(image)
+disp.display()
 
 # Create OBD connection to defined serial port (with baudrate etc)
 c = obd.OBD("/dev/ttyUSB0")
@@ -76,6 +84,29 @@ def percent(messages):
 	v = v * 51 / 46 - 6.4
 	return v
 	
+# Define temperature method to calculate real temperature out of raw data value and abstract battery temperature
+def temperature(messages):
+	h = messages[0].hex()
+	print("Temp hex data:")
+	print(h)
+	ha = h[6:8]
+	print("first hextett")
+	print(ha)
+	ha = int(ha, 16)
+	print("first hextett decimated")
+	print(ha)
+	hb = h[8:10]
+	print("second hextett")
+	print(hb)
+	hb = int(hb, 16)
+	print("second hextett decimated")
+	print(hb)
+	h = ((ha * 256 + hb) / 64)
+	print("calculated result")
+	print(h)
+	h = (h - 320) / 8
+	return h
+	
 # Communicate which car control device you want to talk to
 try:
 	c.interface._ELM327__send(b"ATZ",delay=0)
@@ -84,21 +115,19 @@ try:
 	c.interface._ELM327__send(b"ATH1",delay=0)
 	c.interface._ELM327__send(b"AT CRA 7ED",delay=0)
 	c.interface._ELM327__send(b"AT SH 7E5",delay=0)
-# What happens if connection to car fails
 except:
 	print("Can't connect to car...")
 
-# Loop that continuously checks percentage and power
 while True:
+	# Define custom command to request battery percentage from car
 	try:
-    # Define custom command to request battery percentage from car
 		cmdPerc = OBDCommand("BatteryPercent", "Percentage of HV Battery", b"22028C55555555", 4, percent)	
 		
 		# Query car for battery value
 		response = c.query(cmdPerc, force=True)
 		PERC = response.value
-    
-		# Parse answer from car and write it into variable to show it on screen
+		# Parse answer from car and write it into the file to show it on screen
+		
 		PERC = round(PERC,0)
 		PERC = int(PERC)
 		if PERC < 100:
@@ -109,44 +138,63 @@ while True:
 		# Debug output
 		print(response.value)
 		
-  # What happens if connection to car fails
+		if DEBUG == True:
+			PERC = "" + str(DEBUGPERC) + "%"
+		
 	except:
 		print("Can't connect to car")
 		if DEBUG == False:
 			PERC = "No car!"
-    # Show dummy values on screen if Debugmode is active
 		elif DEBUG == True:
 			PERC = DEBUGPERC
 			PERC = round(PERC,0)
 			PERC = int(PERC)
 			if PERC < 100:
-				PERC = "  " + str(PERC) + "%"
-			else:
 				PERC = " " + str(PERC) + "%"
-  
+			else:
+				PERC = str(PERC) + "%"
+				
+	try:
+		# Define custom command to request battery temperature from car
+		cmdTemp = OBDCommand("BatteryTemperature", "Temperature of HV Battery", b"222a0b55555555", 5, temperature)
+		
+		# Query car for temperature value
+		response = c.query(cmdTemp, force=True)
+		TEMP = response.value
+		
+		# Parse answer from car and write it into variable to show it on screen
+		TEMP = round(TEMP,0)
+		TEMP = int(TEMP)
+		TEMP = str(TEMP) + "˚C"
+		
+		# Debug output
+		print(response.value)
+		
+		if DEBUG == True:
+			TEMP = str(DEBUGTEMP) + "˚C"
+		
+	except:
+		print("Can't connect to car")
+		#if DEBUG == False:
+		TEMP = "Fail!"
+	
 	try: 
-    # Define mode method to to check if car is charging or not out of raw data value from car
 		def mode(messages):
 			e = messages[0].hex()
 			e = e[6:]
 			e = bytes_to_int(e)
 			return e
-		
-    # Define custom command to check if car is charging or not
+		        
 		cmdMode = OBDCommand("BatteryMode", "Mode of HV Battery", b"22744855555555", 4, mode)
 		
-    # Query car for charging, save it to variable and create debugging output
 		response = c.query(cmdMode, force=True)
 		mode = response.value
 		print("Mode: " + str(response.value))
 		
-    # Mode 12337 is not plugged in, mode 12340 is plugged in, possible other modes not (yet) discovered
 		if mode != 12337:
-		  
-      # Set screenmode to switchscreen to show battery percentage and charging power alternating with each update
+		
 			screenmode = "switchscreen"
-			
-      # Define voltage method to calculate battery voltage from raw values from car
+				
 			def voltage(messages):
 				f = messages[0].hex()
 				fa = f[6:8]
@@ -155,8 +203,7 @@ while True:
 				fb = int(fb, 16)
 				f = (fa * 2 ** 8 + fb) / 4
 				return f
-			
-      # Define current method to calculate battery current from raw values from car
+				
 			def current(messages):
 			    g = messages[0].hex()
 			    ga = g[6:8]
@@ -165,70 +212,53 @@ while True:
 			    gb = int(gb, 16)
 			    g = (ga * 2 ** 8 + gb - 2044) / 4
 			    return g
-			
-      # Define custom command to check battery voltage
+			    
 			cmdVolt = OBDCommand("BatteryVoltage", "Voltage of HV Battery", b"221E3B55555555", 0, voltage)
-      # Define custom command to check battery current
 			cmdCurr = OBDCommand("BatteryCurrent", "Current of HV Battery", b"221E3D55555555", 0, current)
 			
-      # Query car for battery voltage, save it into variable and create debugging output
 			response = c.query(cmdVolt, force=True)
 			voltage = response.value
 			print("Voltage: " + str(response.value) + "V")
 			
-      # Query car for battery current, save it into variable and create debugging output
 			response = c.query(cmdCurr, force=True)
 			current = response.value
 			print("Current: " + str(response.value) + "A")
 			
-      # Round charging power to one decimal and always show positive values
 			power = round((abs(voltage * current) / 1000), 1)
 			
-      # Don't show decimal values for charging power over 10kW as it would be too wide for the display
 			if power >= 10:
-				power = " " + str(round((abs(voltage * current) / 1000), 0))
-			
-      # Set variable with kW extension for displaying
+				power = " " + str(int(round((abs(voltage * current) / 1000), 0)))
+				
 			POWER = str(power) + "kW"
 			
-      # Create debugging output
 			print("Power: " + str(power))
-		
-    # Set screenmode to percscreen if batterymode is "not charging"
+			
 		else:
 			screenmode = "percscreen"
-	
-  # What to do if commands for querying mode, voltage or current fail
+					
 	except:
 		print("Can't connect to car: Mode, Voltage or Current not available")
-	
-  # Check screenmode and display corresponding value on screen
+		
 	if screenmode == "percscreen":
 		DISP = PERC
-  # Check screenmode, display corresponding value on screen and switch to next value
 	elif screenmode == "switchscreen" and screen == "perc":
 		DISP = PERC
 		screen = "power"
-  # Check screenmode, display corresponding value on screen and switch to next value
 	elif screenmode == "switchscreen" and screen == "power":
 		DISP = POWER
 		screen = "perc"
-	
-  # Create debugging output for what is shown on screen
+		
 	print("DISP: " + DISP)
 
 	# Draw a black filled box to clear the image.
 	draw.rectangle((0,0,width,height), outline=0, fill=0)
 	
-  # Draw value from variable on screen
 	draw.text((x, top),        str(DISP), font=font, fill=255)
+	if DISP == PERC:
+		draw.text((93, -1), str(TEMP), font=smallfont, fill=255)
 	
 	disp.image(image)
 	disp.display()
-  
-  # Create debugging output which screenmode and screen are shown next
 	print(screenmode)
 	print(screen)
-  
-  # Wait 10 seconds before updating the screen
-	time.sleep(10)
+	time.sleep(7)
